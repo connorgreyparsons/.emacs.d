8f6475a38b34681acfeacf045763206dc2cedb06
connorgreyparsons <cgreyparsons@gmail.com>
2019-03-19 19:59:27 -0500

    replace ido with swiper

--- init.el
+++ init.el
@@ -53,15 +53,56 @@
 :config
 (which-key-mode))
 
-;; ido
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+;; ;; ido
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-everywhere t)
+;; (ido-mode 1)
+
+(use-package counsel
+:ensure t
+)
+
+(use-package swiper
+:ensure try
+:config
+(progn
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-load-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+))
 
 ;; ibuffer
 (defalias 'list-buffers 'ibuffer)
 
+;; winner mode
+(winner-mode 1)
 
+;; ace-window
+;; (use-package ace-window
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (global-set-key [remap other-window] 'ace-window)
+;;     ))
+
+;; window navigation
+(global-set-key (kbd "C-.") 'next-multiframe-window)
+(global-set-key (kbd "C-,") 'previous-multiframe-window)
 
 (use-package color-theme-sanityinc-tomorrow
   :ensure t
@@ -105,6 +146,7 @@
 ;; tern - autocomplete
 (use-package company
   :ensure t)
+
 (use-package company-tern
   :ensure t
   :config
@@ -199,4 +241,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(display-time-24hr-format t)
- '(display-time-day-and-date t))
+ '(display-time-day-and-date t)
+ '(package-selected-packages
+   (quote
+    (counsel tern-auto-complete which-key use-package try treemacs-projectile smartparens rjsx-mode rainbow-delimiters org-bullets company-tern color-theme-sanityinc-tomorrow))))
